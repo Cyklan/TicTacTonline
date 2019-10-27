e7653a6e266c877ba15fc4ecb2384f8efe83da76
NoctusRex <noxjudex@gmail.com>
2019-10-27 17:00:05 +0100

    password zu hash umbennant

--- Server/Database/DatabaseQueries.cs
+++ Server/Database/DatabaseQueries.cs
@@ -23,14 +23,14 @@ namespace Server.Database
 
         public bool LoginUser(User user)
         {
-            if (database.ExecuteQuery("SELECT * FROM users WHERE password='@0' and name='@1';", user.PasswordHash, user.Name).Count != 1) return false;
+            if (database.ExecuteQuery("SELECT * FROM users WHERE hash='@0' and name='@1';", user.PasswordHash, user.Name).Count != 1) return false;
 
             database.ExecuteNonQuery("UPDATE users SET loggedin=1 WHERE name='@1';");
 
             return true;
         }
 
-        public int RegisterUser(User user) => database.ExecuteNonQuery("INSERT INTO users(name, password) VALUES ('@1', '@2');", user.Name, user.PasswordHash);
+        public int RegisterUser(User user) => database.ExecuteNonQuery("INSERT INTO users(name, hash) VALUES ('@1', '@2');", user.Name, user.PasswordHash);
 
         public void Dispose()
         {
