993e1942f709324c784629dab98bb379ff0126e6
NoctusRex <noxjudex@gmail.com>
2019-10-31 11:16:52 +0100

    Websocket Client Hinzugefügt

---
+++ Client/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    public class Converter
+    {
+        public string ConvertBytesToString(byte[] bytes)
+        {
+            return new ASCIIEncoding().GetString(bytes);
+        }
+
+        public T ConvertJsonToObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public string ConvertObjectToJson<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj,Formatting.Indented);
+        }
+
+        public byte[] ConvertStringToBytes(string data)
+        {
+            return new ASCIIEncoding().GetBytes(data);
+        }
+
+    }
+}
